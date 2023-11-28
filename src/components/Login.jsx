897f3568508dc741986c7c06578766656e824394
Anis Shakeel <h.m.anisshakil786@gmail.com>
2023-11-28 19:46:19 +0500

    First Commit.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import Web3 from "web3"; // Install using: npm install web3
 import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert(
+      "This functionality is not available for the time being. Please use MetaMask to log in."
+    );
+  };
+
+  const handleMetaMaskLogin = async () => {
+    // Check if MetaMask is installed
+    if (window.ethereum) {
+      try {
+        // Request account access
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        // Use the first account as the Ethereum address for simplicity
+        const ethereumAddress = accounts[0];
+
+        // Send ethereumAddress to your server for authentication
+        const response = await fetch("/api/authenticate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ethereumAddress }),
+        });
+
+        if (response.ok) {
+          console.log(
+            "Authenticated with MetaMask. Ethereum Address:",
+            ethereumAddress
+          );
+          // If authentication is successful, navigate to the admin page
+          history.push("/admin");
+        } else {
+          console.error("Authentication failed");
+          alert("Authentication failed. Please try again.");
+        }
+      } catch (error) {
+        console.error("Error connecting with MetaMask:", error);
+        alert("Error connecting with MetaMask. Please try again.");
+      }
+    } else {
+      console.log("MetaMask not installed");
+      alert("MetaMask not installed. Please install MetaMask and try again.");
+    }
+  };
+
   return (
     <section>
-      <div class="container">
-        <form action="#" method="post">
-          <h2>login</h2>
+      <div className="container">
+        <form onSubmit={handleLogin}>
+          <h2>Login</h2>
 
-          <div class="inputfield">
-            <input type="text" required="required" />
+          <div className="inputfield">
+            <input
+              type="text"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
             <span>Username</span>
             <i></i>
           </div>
-          <div class="inputfield">
-            <input type="password" required="required" />
+          <div className="inputfield">
+            <input
+              type="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <span>Password</span>
             <i></i>
           </div>
 
           <button type="submit">Log in</button>
-          
-          <div class="alt">
-            <div className="text">Or Sign in with:</div>
-            <button type="button">MetaMask</button>
-          </div>
 
+          <div className="alt">
+            <div className="text">Or Sign in with:</div>
+            <button type="button" onClick={handleMetaMaskLogin}>
+              MetaMask
+            </button>
+          </div>
         </form>
       </div>
     </section>
